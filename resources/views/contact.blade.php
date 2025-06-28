e738b59ce8172dd1c44377b3493b1f7f6eea9909
Shaqayeq Temori <edristemory33@gmail.com>
2025-06-28 11:40:38 +0430

    adding

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,10 +6,25 @@
     @csrf
     <label >Name:</label>
     <input type="text" name="Name" required><br>
+    @error('Name')
+    <div style="color:red;">
+        {{$message}}
+    </div>
+    @enderror
     <label >Email:</label>
     <input type="email" name="Email" required><br>
+    @error('Email')
+    <div style="color:red;">
+        {{$message}}
+    </div>
+    @enderror
     <label >Message:</label>
     <textarea name="message" required></textarea><br>
+    @error('message')
+    <div style="color:red;">
+        {{$message}}
+    </div>
+    @enderror
     <button type="submit">Send</button>
 </form>
 @endsection
